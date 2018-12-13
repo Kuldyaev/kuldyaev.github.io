93691f96e1958727b52f51c16d7454be62606923
Kuldyaev Viacheslav <slavalion1@gmail.com>
2018-12-13 23:25:19 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -192,16 +192,12 @@ addMarkersToMap = (restaurants = self.restaurants) => {
 }
 
 /*   registration  Service Worker  */
-document.addEventListener("DOMContentLoaded", event => {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
-    .then(function(registration) {
-      console.log('Service worker registration succeeded:', registration);
-      };
-    .catch(function(error) {
-      console.log('Service worker registration failed:', error);
-    };
-  } else {
-      console.log('Service workers are not supported.');
-  }
-});
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    console.log('Service worker registration succeeded:', registration);
+  }, /*catch*/ function(error) {
+    console.log('Service worker registration failed:', error);
+  });
+} else {
+  console.log('Service workers are not supported.');
+}
